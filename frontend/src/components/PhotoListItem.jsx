7608f28b29d4d8c583a6c98086a482a596bb7c8c
Ben Hood <benhood97@btinternet.com>
2024-05-20 13:17:21 -0700

    connect scss to photoListItem component

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,13 +6,18 @@ const PhotoListItem = (props) => {
   /* Insert React */
   const sampleData = props.sampleDataForPhotoListItem;
   return (
-    <div id={sampleData.id}>
-      <img src={sampleData.imageSource} alt="Main" className="main-image" />
-      <img src={sampleData.profile} alt="Profile" className="profile-image" />
-      <div className="username">{sampleData.username}</div>
-      <div className="location">
-        {sampleData.location.city}, {sampleData.location.country}
-      </div>
+    <div id={sampleData.id} className="photo-list__item">
+      <img src={sampleData.imageSource} alt="Main" className="photo-list__image" />
+        <div className="photo-list__user-details">
+          <img src={sampleData.profile} alt="Profile" className="photo-list__user-profile" />
+          <div className="photo-list__user-info">
+            {sampleData.username}
+            <div className="photo-list__user-location">
+            {sampleData.location.city}, {sampleData.location.country}
+            </div>
+          </div>
+          
+        </div>
     </div>
   );
 };
