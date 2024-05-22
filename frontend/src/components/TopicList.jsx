0a649208b9a72c2a3aee52d34c0440de62aae732
Ben Hood <benhood97@btinternet.com>
2024-05-21 20:03:44 -0700

    link mock data to app

--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -2,29 +2,12 @@ import React from "react";
 
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
-
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
+import topics from "mocks/topics";
 
 const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
-        {sampleDataForTopicList.map((data) => (
+        {topics.map((data) => (
           <div key={data.id}>
             <TopicListItem data={data} />
           </div>
