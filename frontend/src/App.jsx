1efd3ce52e6642f18745141e5a70512bbbb245d1
Ben Hood <benhood97@btinternet.com>
2024-05-21 19:51:58 -0700

    implement nav bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 import './App.scss';
 import PhotoList from 'components/PhotoList';
 import TopicList from 'components/TopicList';
+import TopNavigation from 'components/TopNavigationBar';
 
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
   return (
     <div className="App">
-      <TopicList />
+      <TopNavigation />
       <PhotoList />
     </div>
   );
